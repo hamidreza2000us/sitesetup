fec5830abf2443d73a5d5861a3099304302f0e8d
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-19 22:09:59 +0430

    Update 01-DNSForwarder.sh

--- 01-DNSForwarder.sh
+++ 01-DNSForwarder.sh
@@ -1,8 +1,8 @@
-#this script setup very base system to be use as dns forwarder, 
+#this script setup very base system to be use as dns forwarder,
 #it also server as primary dns server before IDM is setup
 #After host is setup is finished, setup IDM host and make is primary DNS server for other servers
 #don't forget to use this host as IDM forwarder
-#start with a fresh miniamal RH82 with at least 1 core, 512 MG memory, 20GB disk is enough 
+#start with a fresh miniamal RH82 with at least 1 core, 512 MG memory, 20GB disk is enough
 #don't forget to 1- mount the installation DVD and 2- don't run any DHCP service in the current network
 #do the following manually
 #ip a sh
@@ -11,8 +11,8 @@
 #-----------------------------------
 #ssh to host
 #upload Site01.tar to /root/
-if [ -f ~/Site01.tar ] 
-then 
+if [ -f ~/Site01.tar ]
+then
   tar -xvf ~/Site01.tar
   #copy and paste RH8-BaseSystem.sh to /root/
   sed -i '1s/^.*#/#/;s/\r$//' ~/Site01/RH8-BaseSystem.sh
@@ -26,8 +26,5 @@ then
   mv Site01 /srv/myhost/www/
   restorecon -Rv /srv/myhost/www/
 fi
-#################################################changed
-
-
-
-
+#################################################changed2
+####
