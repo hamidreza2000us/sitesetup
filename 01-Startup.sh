ca90034f6f2ba961e80f72186da3675ff0f11e40
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-20 11:06:36 +0430

    change

--- 01-Startup.sh
+++ 01-Startup.sh
@@ -4,13 +4,14 @@
 #PLEASE note the 0-INTRO.txt and excute the commands needed before running this script#
 #######################################################################################
 echo "We need some input for intial setup. Plase be patient for a few minutes"
-curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-8-BaseParameters.sh -o ~/sitesetup/RH7-8-BaseParameters.sh
-curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-BaseSystem.sh -o ~/sitesetup/RH7-BaseSystem.sh
-bash ~/sitesetup/RH7-8-BaseParameters.sh
-bash ~/sitesetup/RH7-ForemanBaseSystem.sh
+curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-8-BaseParameters.sh -o ~/sitesetup-pre/RH7-8-BaseParameters.sh
+curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-BaseSystem.sh -o ~/sitesetup-pre/RH7-BaseSystem.sh
+bash ~/sitesetup-pre/RH7-8-BaseParameters.sh
+bash ~/sitesetup-pre/RH7-ForemanBaseSystem.sh
 ssh-keygen -t rsa
 ssh-copy-id root@$IDMIP
 echo "There is no other input from your side. So relax. this setup would takes hours to complete"
 git clone https://github.com/hamidreza2000us/sitesetup.git
+cp ~/sitesetup-pre/variables.sh ~/sitesetup/
 bash ~/sitesetup/RH82-DNSMasqSetup.sh
 
