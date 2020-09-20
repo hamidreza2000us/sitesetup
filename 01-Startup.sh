ba3984fe27397e3fe0de9e3e06c7cf90fc40d50f
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-20 15:51:49 +0430

    change

--- 01-Startup.sh
+++ 01-Startup.sh
@@ -8,6 +8,7 @@ curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-8-Ba
 curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/RH7-ForemanBaseSystem.sh -o ~/sitesetup-pre/RH7-ForemanBaseSystem.sh
 curl https://raw.githubusercontent.com/hamidreza2000us/sitesetup/master/variables.sh -o ~/sitesetup-pre/variables.sh
 bash ~/sitesetup-pre/RH7-8-BaseParameters.sh
+ source ~/sitesetup-pre/variables.sh
 bash ~/sitesetup-pre/RH7-ForemanBaseSystem.sh
 ssh-keygen -t rsa
 ssh-copy-id root@$IDMIP
