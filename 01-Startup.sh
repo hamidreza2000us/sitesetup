723259cad25ed8713b4216223307694c4ad3e9a1
hamidreza2000us <66959310+hamidreza2000us@users.noreply.github.com>
2020-09-25 15:03:41 +0330

    change

--- 01-Startup.sh
+++ 01-Startup.sh
@@ -21,6 +21,7 @@ ssh $IDMIP bash ~/sitesetup/02-IDMHostsetup.sh
 bash ~/sitesetup/RH7-8-SetupChronyClient.sh 
 nmcli con mod fixed ipv4.dns $IDMIP
 nmcli con reload
+nmcli con up fixed
 bash ~/sitesetup/CO77-IDMRegister.sh
 #bash ~/sitesetup/CO77-ForemanSetup.sh 
 
